0206dfed3f29d5062a00fbcc7782d3573c18b30b
april <martinezapril56@yahoo.com>
2019-08-08 21:25:11 -0400

    making grid

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Grid extends React.Component {
+    render() {
+        const width = this.props.cols * 14;
+        var rowsArr = [];
+        return (
+<div className= "grid" style={{width: width}}>
+    {{rowsArr}}    
+</div>
+        );
+    }
+}
+
+class Main extends React.Component {
+    constructor(){
+        super();
+        this.speed = 100;
+        this.rows =30;
+        this.cols = 50;
+
+
+        this.state ={
+           generation: 0, 
+            gridFull: Array(this.rows).fill().map(()=>Array(this.cols).fill(false))
+
+
+        }
+    }
+    render( ) {
+        return(
+            <div>
+<h1>The game of Life</h1>
+<Grid
+gridFull={this.state.gridFull}
+rows={this.rows}
+/>
+<h2>Generations:{this.state.generation}</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'));
+
